f02a58a50fa6f0f45c6b6e3e0324f0ea2b6550d1
Elahine Molina <emolina@banreservas.com>
2022-06-28 21:38:22 -0400

    [ADD] Menu

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const prompt = require("prompt-sync")()
-const Range = require("range-dojo")
+const {Range} = require("range-dojo")
 
 let arr = prompt("Insert the desired range with the following structure (/[x, y]/):  ")
 const r = new Range(arr)
 
 console.log("Options:")
-console.log("1. \n")
+console.log("1. Contains Integer\n2. Contains Range\n3. Get All Points\n4. Equals\n5. Get Endpoints ")
 let opt = prompt("Which action would you lik to do: ");
 
 switch (opt) {
