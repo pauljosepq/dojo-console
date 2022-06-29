e296825148a388c8f71e75eb2e401b9a200bbe06
Paul Paniagua <70442497+pauljosepq@users.noreply.github.com>
2022-06-29 01:48:37 +0000

    [IMP] Finish app

--- index.js
+++ index.js
@@ -6,13 +6,40 @@ const r = new Range(arr)
 
 console.log("Options:")
 console.log("1. Contains Integer\n2. Contains Range\n3. Get All Points\n4. Equals\n5. Get Endpoints ")
-let opt = prompt("Which action would you lik to do: ");
+let opt = prompt("Which action would you like to do: ");
 
 switch (opt) {
-    case value:
-        
+    case '1':
+        let integer = prompt("Insert the intergers separated by commas: ")
+        if(r.contains(integer)){
+            console.log("The range does contains the interger/s")
+        }else{
+            console.log("The range does not contains the interger/s")
+        }
         break;
-
+    case '2':
+        let range = prompt("Insert the range to evaluate with the following structure (/[x, y]/):  ")
+        if(r.containsRange(range)){
+            console.log("The range does contains the given range")
+        }else{
+            console.log("The range does not contains given range")
+        }
+        break;
+    case '3':
+        console.log(r.getAllPoints())
+        break;
+    case '4':
+        let equality = prompt("Insert the range to evaluate equality with the following structure (/[x, y]/):  ")
+        if(r.equals(equality)){
+            console.log("The range is equal to the given range")
+        }else{
+            console.log("The range is not equal to the given range")
+        }
+        break;
+    case '5':
+        console.log(r.getEndPoints())
+        break;
     default:
+        console.log("The specified option is not defined")
         break;
 }
