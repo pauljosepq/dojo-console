720a3e46c0e0d492bbc97d1b5babdbc25f18aa4b
VToribiot <vtoribiot@gmail.com>
2022-06-28 20:36:25 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const prompt = require("prompt-sync")()
+const Range = require("range-dojo")
+
+let arr = prompt("Insert the desired range with the following structure (/[x, y]/):  ")
+const r = new Range(arr)
+
+console.log("Options:")
+console.log("1. \n")
+let opt = prompt("Which action would you lik to do: ");
+
+switch (opt) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
